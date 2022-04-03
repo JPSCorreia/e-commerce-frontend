a19b800c014751cdcd9d4805a72ef5b437a6bb10
João Correia <jpcorreia@mail.com>
2022-04-03 23:43:25 +0100

    Connect login to backend

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,5 @@
 import '../Style/App.css';
+import {useState} from 'react';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -13,17 +14,20 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useAuthUserMutation } from '../Features/authApiSlice';
 
 function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const [authUser] = useAuthUserMutation();
+
 
   const [values, setValues] = React.useState({
     password: '',
     showPassword: false,
   });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
+  // const handleChange = (prop) => (event) => {
+  //   setValues({ ...values, [prop]: event.target.value });
+  // };
 
   const handleClickShowPassword = () => {
     setValues({
@@ -38,16 +42,18 @@ function LoginForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    // const data = new FormData(event.currentTarget.data);
+
+    console.log(event)
 
     await authUser({
-      username: data.get('username'),
-      password: data.get('password'),
+      username,
+      password,
     });
 
     console.log({
-      username: data.get('username'),
-      password: data.get('password'),
+      username,
+      password,
     });
   }
 
@@ -68,6 +74,8 @@ function LoginForm() {
       <OutlinedInput
         id="outlined-required"
         label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.currentTarget.value)}
         endAdornment={
           <InputAdornment position="end">
             <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
@@ -82,8 +90,8 @@ function LoginForm() {
         <OutlinedInput
           id="outlined-adornment-password"
           type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -98,12 +106,14 @@ function LoginForm() {
           }
           label="Password"
         />
+        
       </FormControl> 
 
     </Box>
     <Button
       type="submit"
       variant="contained"
+      onClick={handleSubmit}
       sx={{ mt: 3, mb: 2 }}
       >Sign In
     </Button>
