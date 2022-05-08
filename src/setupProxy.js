e2ef20668fb5ba5a6fac0983c5e30b505fe3a2ec
João Correia <jpcorreia@mail.com>
2022-05-08 18:26:06 +0100

    Remove proxy port

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware').createProxyMiddleware;
 
 module.exports = function (app) {
-    app.use(proxy(`/auth/**`, { target: 'https://e-commerce-backend2.herokuapp.com:51382' }));
+    app.use(proxy(`/auth/**`, { target: 'https://e-commerce-backend2.herokuapp.com' }));
 };
