16a644242d97d392829dca47c28756ca6f402c78
João Correia <jpcorreia@mail.com>
2022-06-06 08:05:01 +0100

    1

--- src/Components/Orders/OrderList.js
+++ src/Components/Orders/OrderList.js
@@ -1,40 +1,13 @@
 import '../../Style/App.css';
 import * as React from 'react';
 import { Box, List } from '@chakra-ui/react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { api } from '../../Features/routes';
+import { useSelector } from 'react-redux';
 import Order from './Order';
-import { useAuth0 } from "@auth0/auth0-react";
-import Loader from '../Loader';
 
 function OrderList() {
 
   // React/Redux State/Action Management.
-  const dispatch = useDispatch();
   const orderData = useSelector((state) => state.orderData.allOrdersData || [])
-  const orderDataIsLoading = useSelector((state) => state.orderData.allOrdersDataIsLoading)
-  const { user, getAccessTokenSilently } = useAuth0();
-
-  useEffect(() => {
-    const getData = async () => {
-      const token = process.env.REACT_APP_IN_DEVELOPMENT? 'dev token' :
-      await getAccessTokenSilently({
-       audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-       scope: 'openid'
-     })
-      const orderObj = {
-        token: token,
-        email: user.email
-      }
-      await dispatch(api.orders.getAllOrders(orderObj)) 
-    }
-    getData();
-  }, [dispatch, getAccessTokenSilently, user.email]);
-
-  if (orderDataIsLoading) {
-    return <Loader />
-  }
 
   return(
     <Box className='order-list'>
--- src/Components/Orders/OrdersPage.js
+++ src/Components/Orders/OrdersPage.js
@@ -9,12 +9,31 @@ import {
   BreadcrumbLink,
 } from '@chakra-ui/react'
 import {ChevronRightIcon} from '@chakra-ui/icons';
-
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { api } from '../../Features/routes';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function OrdersPage() {
 
   const themeColor = useColorModeValue('blue.500', 'blue.200');
+  const dispatch = useDispatch();
+  const { user, getAccessTokenSilently } = useAuth0();
 
+  useEffect(() => {
+    const getData = async () => {
+      const token = process.env.REACT_APP_IN_DEVELOPMENT? 'dev token' :
+      await getAccessTokenSilently({
+       audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+       scope: 'openid'
+     })
+      await dispatch(api.orders.getAllOrders({
+        token: token,
+        email: user.email
+      })) 
+    }
+    getData();
+  }, []);
 
   return(
     <Box className='order-page' >
